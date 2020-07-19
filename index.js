4cd537be6cf710689f380aed05f55956bb3d27f0
Jonathan Neves <nevesjon98@gmail.com>
2020-07-19 13:24:11 -0300

    Removido comentário index

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 import { AppRegistry } from 'react-native';
 
 import App from './App';
-//import App from './src/screens/Feed';
-//import App from './src/screens/WebViewNews';
 import Style from './src/styles/Style'
 
 global.fontColor = Style.fontDark;
